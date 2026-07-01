c21a4c0c3ee9619c7f1c041990e3871d13a87d75
Chloe <chloelizabeth.com@gmail.com>
2026-07-01 17:06:36 -0400

    Created insert function within Trie class

--- trieClass.py
+++ trieClass.py
@@ -6,3 +6,16 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+
+    def insert(self, word):
+        curr = self.root
+        for char in word:
+            # Check if character is already in the Trie
+            index = ord(char) - ord('a')
+            # If not, create a new node
+            if not curr.children[index]:
+                curr.children[index] = TrieNode()
+            # Move curr to point to the new node
+            curr = curr.children[index]
+        # Flag the end of the word as true
+        curr.isEndofWord = True
