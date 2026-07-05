90f381790cb13a2e9215c59c44314a4ea04932b7
Chloe <chloelizabeth.com@gmail.com>
2026-07-05 14:50:49 -0400

    Made words reversed for insertion and completed Insert Function in Trie Class

--- trieClass.py
+++ trieClass.py
@@ -2,13 +2,22 @@ class TrieNode:
     def __init__(self):
         self.children = [None] * 26
         self.isEndofWord = False
+        self.word = None
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word):
+        # Point to the root
         curr = self.root
+        # Save the original word
+        originalWord = word
+        # Make the word lowercase
+        word = word.lower()
+        # Reverse the word (this way finding the ends of words that match will be easier to determine words that rhyme)
+        word = word[::-1]
+        # Iterate through each character in word
         for char in word:
             # Check if character is already in the Trie
             index = ord(char) - ord('a')
@@ -19,3 +28,4 @@ class Trie:
             curr = curr.children[index]
         # Flag the end of the word as true
         curr.isEndofWord = True
+        curr.word = originalWord
